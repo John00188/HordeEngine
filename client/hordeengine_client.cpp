1680da3a61cd4e1b2fd7ff39f6151a7e73b8c300
John00188 <johnsonnier81@gmail.com>
2025-08-29 15:27:44 -0500

    Add client and server build targets

---
+++ client/hordeengine_client.cpp
@@ -0,0 +1,28 @@
+#include <GarrysMod/Lua/Interface.h>
+using namespace GarrysMod::Lua;
+
+LUA_FUNCTION(he_GetStatus)
+{
+    LUA->PushString("hordeengine: client ok");
+    return 1;
+}
+
+LUA_FUNCTION(he_Bench)
+{
+    LUA->PushNumber(0.0);
+    return 1;
+}
+
+GMOD_MODULE_OPEN()
+{
+    LUA->PushSpecial(SPECIAL_GLOB);
+    LUA->PushCFunction(he_GetStatus); LUA->SetField(-2, "he_GetStatus");
+    LUA->PushCFunction(he_Bench);     LUA->SetField(-2, "he_Bench");
+    LUA->Pop();
+    return 0;
+}
+
+GMOD_MODULE_CLOSE()
+{
+    return 0;
+}
