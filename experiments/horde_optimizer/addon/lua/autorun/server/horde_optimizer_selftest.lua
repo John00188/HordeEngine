6cce0ca0560a4aac5b8884dfe9985c8245ca395d
John Sonnier <johnsonnier81@gmail.com>
2025-08-30 21:36:15 -0500

    sandbox: add drop-in Horde Optimizer addon + CODEX task + pack/install scripts

---
+++ experiments/horde_optimizer/addon/lua/autorun/server/horde_optimizer_selftest.lua
@@ -0,0 +1,8 @@
+﻿if not SERVER then return end
+timer.Simple(5, function()
+    if ConVarExists("ho_enabled") then
+        print("[HO] selftest: ho_enabled=", GetConVar("ho_enabled"):GetBool())
+    else
+        print("[HO] selftest: ConVars missing?")
+    end
+end)
---
+++ experiments/horde_optimizer/install_to_gmod.ps1
@@ -0,0 +1,7 @@
+﻿param()
+$gm = "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod"
+$src = "C:\Users\John Sonnier\Desktop\HordeEngine_Improvementinator_fixed\experiments\horde_optimizer\addon"
+$dst = Join-Path $gm "addons\horde_optimizer"
+if (Test-Path $dst) { Remove-Item $dst -Recurse -Force }
+robocopy $src $dst /E | Out-Null
+Write-Host "Installed -> $dst"
