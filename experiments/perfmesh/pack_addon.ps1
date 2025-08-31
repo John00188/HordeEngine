a87423cf66b2f4fdc53cfb833b72f7fcbf56aae7
John Sonnier <johnsonnier81@gmail.com>
2025-08-30 22:09:11 -0500

    perfmesh: add inter-addon mesh (pubsub, scheduler, claims, services) + helpers

---
+++ experiments/perfmesh/pack_addon.ps1
@@ -0,0 +1,5 @@
+﻿param()
+$zipOut = Join-Path "C:\Users\John Sonnier\Desktop\HordeEngine_Improvementinator_fixed\artifacts" "perfmesh.zip"
+if (Test-Path $zipOut) { Remove-Item $zipOut -Force }
+Compress-Archive -Path "C:\Users\John Sonnier\Desktop\HordeEngine_Improvementinator_fixed\experiments\perfmesh\addon\*" -DestinationPath $zipOut
+Write-Host "Packed -> $zipOut"
