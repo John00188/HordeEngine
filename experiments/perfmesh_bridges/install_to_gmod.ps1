82153a4a8cbd0530b822b26beade4272ddf17308
John Sonnier <johnsonnier81@gmail.com>
2025-08-30 22:16:10 -0500

    perfmesh-bridges: scaffold loader + per-addon bridge stubs

---
+++ experiments/perfmesh_bridges/install_to_gmod.ps1
@@ -0,0 +1,6 @@
+﻿param()
+$src = "C:\Users\John Sonnier\Desktop\HordeEngine_Improvementinator_fixed\experiments\perfmesh_bridges\addon"
+$dst = Join-Path "E:\SteamLibrary\steamapps\common\GarrysMod\garrysmod" "addons\perfmesh_bridges"
+if (Test-Path $dst) { Remove-Item $dst -Recurse -Force }
+robocopy $src $dst /E | Out-Null
+Write-Host "Installed -> $dst"
